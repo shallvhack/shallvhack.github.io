feeb008a72309d0937a3b9c95ade84909c1b0f77
mymadhavyadav07 <mymadhavyadav07@gmail.com>
2023-06-25 09:19:58 +0530

    updated old website

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,5 +1,6 @@
 "use strict";
 
+console.log("Yeah!!!!!!!");
 (function ($) {
   jQuery(document).ready(function () {
 
@@ -10,26 +11,48 @@
     $(document).scroll(function () {
       if ($(document).scrollTop() > 40) {
         $('#top-navbar').removeClass('initial-navbar');
-        $('#top-navbar').addClass('final-navbar shadow');
-
-        $('#navbar-toggler').removeClass('navbar-dark');
-        $('#navbar-toggler').addClass('navbar-light');
-
-        // get the main logo from hidden img tag
-        let mainLogo = document.getElementById("main-logo").getAttribute("src");
-        $('#logo').attr("src", mainLogo);
-      } else {
-        $('#top-navbar').removeClass('final-navbar shadow');
+        $('#top-navbar').addClass('final-navbar');
+        document.getElementById("logo").src = "./logo_ShallvHack_final_v1-02.png";
+        document.getElementById("logo-anchor").href = "./logo_ShallvHack_final_v1-02.png";
+      }
+      else{
+        $('#top-navbar').removeClass('final-navbar');
         $('#top-navbar').addClass('initial-navbar');
-
-        $('#navbar-toggler').removeClass('navbar-light');
-        $('#navbar-toggler').addClass('navbar-dark');
-
-        // get the inverted logo from hidden img tag
-        let invertedLogo = document.getElementById("inverted-logo").getAttribute("src");
-        $('#logo').attr("src", invertedLogo);
+        document.getElementById("logo").src = "./logo_ShallvHack_final_v1-01.png";
+        document.getElementById("logo-anchor").href = "./logo_ShallvHack_final_v1-01.png";
       }
     });
   });
 
 })(jQuery);
+
+
+// (function ($) {
+//     jQuery(document).ready(function () {
+  
+//       // change navbar style on scroll
+//       // ==================================================
+//       // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+//       // $.onscroll = function() {scrollFunction()};
+//       $(document).scroll(function () {
+//         if ($(document).scrollTop() > 90) {
+//             console.log("working");
+//             document.getElementById("about-header").style.animationPlayState = "running";
+//         }
+//         else{
+//             document.getElementById("about-header").style.animationPlayState = "paused";
+//         }
+//       });
+//     });
+  
+//   })(jQuery);
+  
+  
+  
+
+
+
+
+
+
+
